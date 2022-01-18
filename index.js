9a97fb9d040cac3f1db235508819123cc28b3141
Akshay-Andhale <akshayandhale9595@gmail.com>
2022-01-18 15:49:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+setInterval(function(){
+    temp = new Date()
+    htime = temp.getHours()
+    mtime = temp.getMinutes()
+    stime = temp.getSeconds()
+
+    hrotation = 30*htime + mtime/2
+    mrotation = 6*mtime
+    srotation = 6*stime
+
+    hour.style.transform = `rotate(${hrotation}deg)`
+    minute.style.transform = `rotate(${mrotation}deg)`
+    second.style.transform = `rotate(${srotation}deg)`
+},1000)
